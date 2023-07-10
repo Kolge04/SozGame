88166d6f0351f14c9385d119d3b58e50f28c4cba
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-07-10 10:41:04 +0400

    Update yakalayıcı.py

--- kelime_bot/plugins/yakalayıcı.py
+++ kelime_bot/plugins/yakalayıcı.py
@@ -1,1 +1,81 @@
+import heroku3
+import random
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.events import StopPropagation
+from kelime_bot import kolge as client
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+ 
+ 
+anlik_calisan = []
+ 
+gece_tag = []
+ 
+ 
+
+
+
+
+
+@client.on(events.NewMessage(pattern="^/tag ?(.*)"))
+async def mentionall(event):
+  global gece_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{noadmin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın!__")
+  else:
+    return await event.respond("__Tağ etməy üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Tağ Başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                      Button.url('📣 Support', f'https://t.me/{support}')
+                      ]
+                    )
+                  ) 
+    gece_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"➢ [{usr.first_name}](tg://user?id={usr.id})\n "
+      if event.chat_id not in gece_tag:
+        await event.respond("⛔ Tağ Prosesi Dayandırıldı",
+                    buttons=(
+                      [
+                       Button.url('📣 Support', f'https://t.me/{support}')
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+ 
