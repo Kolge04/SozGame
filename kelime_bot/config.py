6f663caa868b3bca4e33fa826887b26b106193da
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-07-10 09:51:31 +0400

    Create config.py

---
+++ kelime_bot/config.py
@@ -0,0 +1,12 @@
+import os
+ 
+class Config:
+ 
+   API_ID = int(os.getenv("API_ID", "12210813"))
+   API_HASH = os.getenv("API_HASH", "e42eeae11a2f96bcfc5ec3b46a30adad")
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "6062022905:AAF3kB_R5vLKt6HyZfkHx8NcLaMtG9Hji8I")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "***")
+   OWNER_ID = int(os.environ.get("OWNER_ID","****"))
+   OWNER_NAME = os.environ.get("OWNER_NAME", "****")
+   SUPPORT = os.environ.get("SUPPORT", "***")
+   BOT_NAME = os.environ.get("BOT_NAME", "****")
