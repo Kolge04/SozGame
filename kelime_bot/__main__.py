66d8ae38bf474762bb8ba4324d189a07da090dc1
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-07-10 10:32:00 +0400

    Update __main__.py

--- kelime_bot/__main__.py
+++ kelime_bot/__main__.py
@@ -1,6 +1,4 @@
-from kelime_bot import kolge as app
+from kelime_bot import kolge as client
 
- 
-app.start()
-LOGGER.info(f"Uğurla Başladı Sahibim ")
-idle()
+print(">> Bot aktifdi bot hakda məlumatı @sesizKOLGE dan ala bilərsən Versiya 1.7.5<<")
+client.run_until_disconnected()
