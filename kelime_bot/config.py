ff8d125cc3909bf73d2499fc21e298756607d893
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-17 11:29:11 -0800

    Update config.py

--- kelime_bot/config.py
+++ kelime_bot/config.py
@@ -4,10 +4,10 @@ class Config:
  
    API_ID = int(os.getenv("API_ID", "12210813"))  # API-ID YAZ
    API_HASH = os.getenv("API_HASH", "e42eeae11a2f96bcfc5ec3b46a30adad") # API-HASH YAZ
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "6062022905:AAF3kB_R5vLKt6HyZfkHx8NcLaMtG9Hji8I") # BOT-TOKEN YAZ
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # BOT-TAG ADIN YAZ
-   OWNER_ID = int(os.environ.get("OWNER_ID","")) # OZ ID NI YAZ
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "5884794395:AAGXZ25-4BvlJzw2HB08TbwrJPbFwljl2Q8") # BOT-TOKEN YAZ
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "XAOS_Chatbot") # BOT-TAG ADIN YAZ
+   OWNER_ID = int(os.environ.get("OWNER_ID","5663585448")) # OZ ID NI YAZ
    OWNER_NAME = os.environ.get("OWNER_NAME", "sesizKOLGE") # OZ TAG ADINI YAZ
-   KANAL = os.environ.get("KANAL", "") #  KANALIN TAGİN YAZ
-   KANAL_ADI = os.environ.get("KANAL", "") #  KANALIN ADIN YAZ
-   BOT_NAME = os.environ.get("BOT_NAME", "") # BOT ADINI YAZ
+   KANAL = os.environ.get("KANAL", "sesizKOLGE") #  KANALIN TAGİN YAZ
+   KANAL_ADI = os.environ.get("KANAL", "♥🌹💸") #  KANALIN ADIN YAZ
+   BOT_NAME = os.environ.get("BOT_NAME", "𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋") # BOT ADINI YAZ
