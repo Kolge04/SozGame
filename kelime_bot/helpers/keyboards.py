a5264ea69ead4050bd8a5e7a2909230f0c513fca
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-17 11:20:05 -0800

    Update and rename keyboards.py to sd.py

--- kelime_bot/helpers/keyboards.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("📚 Ə𝐌𝐑𝐋Ə𝐑" , url= "t.me/XAOS_Gamebot?help")]
-])
-
-
