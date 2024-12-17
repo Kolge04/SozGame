9a1f6abd9c1c53f774356a04dc74bf321b08e182
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-17 10:32:53 -0800

    Update config.py

--- kelime_bot/config.py
+++ kelime_bot/config.py
@@ -2,11 +2,11 @@ import os
  
 class Config:
  
-   API_ID = int(os.getenv("API_ID", "12210813"))
-   API_HASH = os.getenv("API_HASH", "e42eeae11a2f96bcfc5ec3b46a30adad")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "6062022905:AAF3kB_R5vLKt6HyZfkHx8NcLaMtG9Hji8I")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "***")
-   OWNER_ID = int(os.environ.get("OWNER_ID","12345678"))
-   OWNER_NAME = os.environ.get("OWNER_NAME", "sesizKOLGE")
-   SUPPORT = os.environ.get("SUPPORT", "salamsagil")
-   BOT_NAME = os.environ.get("BOT_NAME", "test")
+   API_ID = int(os.getenv("API_ID", "12210813"))  # API-ID YAZ
+   API_HASH = os.getenv("API_HASH", "e42eeae11a2f96bcfc5ec3b46a30adad") # API-HASH YAZ
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "6062022905:AAF3kB_R5vLKt6HyZfkHx8NcLaMtG9Hji8I") # BOT-TOKEN YAZ
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # BOT-TAG ADIN YAZ
+   OWNER_ID = int(os.environ.get("OWNER_ID","")) # OZ ID NI YAZ
+   OWNER_NAME = os.environ.get("OWNER_NAME", "sesizKOLGE") # OZ TAG ADINI YAZ
+   SUPPORT = os.environ.get("SUPPORT", "") # SPORT KANALI YAZ
+   BOT_NAME = os.environ.get("BOT_NAME", "") # BOT ADINI YAZ
