b5926de9028ac85edaa377174913404480a8c1a8
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-17 10:28:37 -0800

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -1,23 +1,42 @@
+
+# @sesizKOLGE
+# Sahib @sesizKOLGE
+# Repo Açığdısa İcazəsis Götürmə Oğlum
+# Oz adina cixardanin anasin sikim
+# hecneye el vurma bele isled !
+
+from time import sleep
+from pyrogram import Client
+import logging
+from dotenv import load_dotenv, set_key, unset_key
 from os import getenv
 from kelime_bot.config import Config
 from telethon import TelegramClient
-from time import sleep
-import logging
-from dotenv import load_dotenv, set_key, unset_key
- 
- 
+
+load_dotenv('config.env')
+
+# THE LOGGING
+
+  
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+
 LOGGER = logging.getLogger(__name__)
  
- 
- 
+ # KOLGE 
+pxaos = Client(
+    'AylinRobot',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
+
+# Telethon
 api_id = Config.API_ID
 api_hash = Config.API_HASH
 bot_token = Config.BOT_TOKEN
  
-kolge = TelegramClient('tag', api_id, api_hash).start(bot_token=bot_token)
- 
- 
+txaos = TelegramClient('Xaos', api_id, api_hash).start(bot_token=bot_token)
+
+
