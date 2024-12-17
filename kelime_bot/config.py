79ed23130a635ce2dcc21e8ea5152c99bf7c28f3
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-18 00:19:55 +0400

    Update config.py

--- kelime_bot/config.py
+++ kelime_bot/config.py
@@ -4,7 +4,7 @@ class Config:
  
    API_ID = int(os.getenv("API_ID", "12210813"))  # API-ID YAZ
    API_HASH = os.getenv("API_HASH", "e42eeae11a2f96bcfc5ec3b46a30adad") # API-HASH YAZ
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "5884794395:AAGXZ25-4BvlJzw2HB08TbwrJPbFwljl2Q8") # BOT-TOKEN YAZ
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "5890868081:AAFJI6-Q3LX8zm7PaYBWmTmz13G7Nc7mEEY") # BOT-TOKEN YAZ
    BOT_USERNAME = os.environ.get("BOT_USERNAME", "XAOS_Chatbot") # BOT-TAG ADIN YAZ
    OWNER_ID = int(os.environ.get("OWNER_ID","5663585448")) # OZ ID NI YAZ
    OWNER_NAME = os.environ.get("OWNER_NAME", "sesizKOLGE") # OZ TAG ADINI YAZ
