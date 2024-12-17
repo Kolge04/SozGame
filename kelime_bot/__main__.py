d41029f807ef186d9fc06c0f89cd03468898d103
Rəddol <117319718+Kolge04@users.noreply.github.com>
2024-12-17 11:22:38 -0800

    Update __main__.py

--- kelime_bot/__main__.py
+++ kelime_bot/__main__.py
@@ -1,4 +1,4 @@
-from kelime_bot import kolge as client
+from kelime_bot import txaos as client
 
 print(">> Bot aktifdi bot hakda məlumatı @sesizKOLGE dan ala bilərsən Versiya 1.7.5<<")
 client.run_until_disconnected()
